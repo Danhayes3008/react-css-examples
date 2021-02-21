159629b0b86d8afb790bab2bc8ad66fe6d7ee825
danhayes <danhayes3008@outlook.com>
2021-02-21 16:16:42 +0000

    initial commit of the toggles file

---
+++ src/components/toggles.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Night = () => {
+  return (
+    <label class="switch">
+        <input type="checkbox" id="night" />
+        <span class="slider round"></span>
+      </label>
+    );
+};
+export default Night;
