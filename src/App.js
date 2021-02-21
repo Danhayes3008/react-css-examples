ce6c737fdec711070e751a6dcf740e3277a79837
danhayes <danhayes3008@outlook.com>
2021-02-21 16:17:50 +0000

    tidy up
    removed the default content that the file came with and added my own

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 import Header from "./components/header.js";
+import Night from "./components/toggles.js";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <p>Warning the page may take some time to fully load. Please scroll slowly</p>
+      <p>
+        Warning the page may take some time to fully load. Please scroll slowly
+      </p>
+      <Night />
     </div>
   );
 }
