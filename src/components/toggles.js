d9e17fc0b7271f30e061b283e5e6df712d6f0af2
danhayes <danhayes3008@outlook.com>
2021-02-23 14:11:17 +0000

    cleaned up the code as i am unable to get the toggle checkbox to do what i wish

--- src/components/toggles.js
+++ src/components/toggles.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
+const Night = (onClick) => {
 
-const Night = () => {
   return (
-    <label class="switch">
-        <input type="checkbox" id="night" />
-        <span class="slider round"></span>
+    <label className="switch">
+          <input type="checkbox" id="chk" />
+        <span className="slider round"></span>
       </label>
     );
 };
